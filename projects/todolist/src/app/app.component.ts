fa48cd99506b29bb84e971055e03cffce3a08768
Axel Laude <axel2003@outlook.com>
2023-12-08 17:31:05 +0100

    Ajout de l'affichage conditionnel sur la todolist

--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TODOS } from './mock-todo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template : `
-  <h1>Hello world</h1>
+  <h1>Liste des choses à faire</h1>
+  <ul *ngFor="let t of todolist">
+    <li *ngIf="!t.isCompleted">
+      <article>
+        <input type="checkbox">{{t.content}}
+      </article>
+  </li>
+    
+  </ul>  
   `,
   styles : []
 
 })
 export class AppComponent {
-  title = 'todolist';
+
+  todolist = TODOS;
+
+  constructor(){
+    console.table(this.todolist);
+
+    
+  }
+
+  selectTodo(id : number){
+    return this.todolist[id].id
+  }
+  
 }
